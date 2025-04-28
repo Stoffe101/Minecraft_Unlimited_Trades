c0129a471a6e0d7fb39f502b8e354e1e5e40996f
Christoffer <cloofsweden@gmail.com>
2025-04-29 01:50:53 +0200

    Initial commit

---
+++ src/client/java/com/example/unlimitedvillagertrades/client/UnlimitedVillagerTradesClient.java
@@ -0,0 +1,10 @@
+package com.example.unlimitedvillagertrades.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UnlimitedVillagerTradesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/example/unlimitedvillagertrades/UnlimitedVillagerTrades.java
@@ -0,0 +1,16 @@
+package com.example.unlimitedvillagertrades;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UnlimitedVillagerTrades implements ModInitializer {
+    public static final String MOD_ID = "unlimitedvillagertrades";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Unlimited Villager Trades initialized! All villager trades will now have unlimited uses.");
+    }
+}
